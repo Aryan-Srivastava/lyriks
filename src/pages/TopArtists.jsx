802239efa6d4f7dfdd2d1fe9828114a624282d29
Aryan <aryanyash999@gmail.com>
2022-11-19 00:36:32 +0530

    changed top artist card section, now displaying artist's picture instead of song cover

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -8,16 +8,18 @@ const TopArtists = () => {
 
   if (error) return <Error />;
 
+  const topArtists = data?.slice(0, 30);
+
   return (
     <div className="dlex flex-col">
       <h2 className="font-bold text-3xl text-white text-left mt-4 mb-4 ">
         Top Aritst
       </h2>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8 ">
-        {data?.map((track, artist) => (
+        {topArtists?.map((artist) => (
           <ArtistCard
-            key={track.key}
-            track={track}
+            key={artist.key}
+            track={artist}
             artist={artist}
             data={data}
           />
